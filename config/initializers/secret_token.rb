7685aec60fd93a6d3545939c799fe929bcefcf96
Boris Nadykto <borisnadykto@mail.ru>
2013-10-12 17:04:51 +0300

    Первый релиз

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Psybook::Application.config.secret_token = 'ffa2dc941bf0f833eb624a7a7c9bf467135610f0c9e6a5ac77cdab887c8eb671fcf0527934d83c8136c559c38bfb9b273d4b4aba521cbab9999c1fd2bba9c4d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Psybook::Application.config.session_store :cookie_store, key: '_psybook_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Psybook::Application.config.session_store :active_record_store
