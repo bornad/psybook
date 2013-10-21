4d9c1cccc0a0917ef91026f8b732e7a9dc9919ff
Boris Nadykto <borisnadykto@mail.ru>
2013-10-21 17:35:02 +0300

    Added friendships

--- test/unit/user_friendship_test.rb
+++ test/unit/user_friendship_test.rb
@@ -1,11 +1,16 @@
 require 'test_helper'
 
 class UserFriendshipTest < ActiveSupport::TestCase
-  should belong_to(:user)
-  should belong_to(:friend)
+	should belong_to(:user)
+	should belong_to(:friend)
 
-  test "that creating a friendship works" do
-    UserFriendship.create user: users(:jason), friend: users(:mike)
-    assert users(:jason).friends.include?(users(:mike))
-  end
+	test "that creating a friendship works without raising an exeception" do
+	  assert_nothing_raised do
+		UserFriendship.create user: users(:jason), friend: users(:mike)
+      end
+   end
+    test "is friendship based on id" do
+     UserFriendship.create user_id: users(:jason).id, friend_id: users(:mike).id
+     assert users(:jason).friends.include?(users(:mike))
+    end
 end
